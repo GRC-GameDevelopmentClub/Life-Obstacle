6917678f9241be2dd37fceb8c93b6249f335d3d2
Cohav0310 <36090325+Cohav0310@users.noreply.github.com>
2019-05-03 17:02:01 -0700

    created player movement script
    created script and added prefabs folder

---
+++ Life-Obstacle/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public GameObject playerObject;
+    public GameObject[] railPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //railPoints = GameObject.FindGameObjectsWithTag("RailPoints");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        playerObject.transform.position = 
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+        }
+        
+    }
+}
