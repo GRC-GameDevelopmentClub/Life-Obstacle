b921eacdbe8bb2b4e2fc80b43eb2212154589e16
Howarddful123 <Howarddful123@gmail.com>
2019-05-17 16:17:07 -0700

    -Added wall moving down(still very glitchy)

--- Life-Obstacle/Assets/Scripts/WallManager.cs
+++ Life-Obstacle/Assets/Scripts/WallManager.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WallManager : MonoBehaviour {
+    public float backgroundSize;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private float viewZone = 10;
+    private int upIndex;
+    private int lowerIndex;
+    // Use this for initialization
+    void Start() {
+        layers = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            layers[i] = transform.GetChild(i);
+        }
+        lowerIndex = 0;
+        upIndex = layers.Length - 1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GoDown();
+        }
+    }
+
+    private void GoDown()
+    {
+        int lastUpIndex = upIndex;
+        layers[upIndex].position = Vector3.down * (layers[lowerIndex].position.y - backgroundSize);
+        lowerIndex = upIndex;
+        upIndex--;
+        if (upIndex < 0)
+        {
+            upIndex = layers.Length - 1;
+        }
+    }
 }
